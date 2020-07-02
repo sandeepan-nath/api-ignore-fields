42a4b1f7b7565c44241233b0c0f11c062354c232
Sandeepan <sandeepan.nath@payufin.com>
2020-07-02 10:22:03 +0530

    init

---
+++ src/main/java/com/baeldung/students/Student.java
@@ -0,0 +1,61 @@
+package com.baeldung.students;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+    
+    private long id;
+    
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String firstName;
+    
+    @JsonIgnore
+    private String lastName;
+    
+    public Student() {}
+    
+    public Student(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        
+        this.lastName = lastName;
+    }
+    
+    public Student(long id, String firstName, String lastName) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+
+}
